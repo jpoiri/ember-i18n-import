10d823348855eac288d7f6028c6e1428cb33e64f
jpoiri <justin.poirier@gmail.com>
2017-06-17 22:35:54 -0300

    Update code to support the different options

--- ember-i18n-import.js
+++ ember-i18n-import.js
@@ -35,78 +35,145 @@ if (!program.inputFile) {
 	throw new Error(chalk.red('The input file must be specified.'));
 }
 
-if (!program.outputDir) {
+if (program.outputDir) {
 	outputDir = program.outputDir;
 }
 
-if (!program.outputFile) {
+if (program.outputFile) {
 	outputFile = program.outputFile;
 }
 
-if (!program.translationKeyColumnName) {
+if (program.translationKeyColumnName) {
 	translationKeyColumnName = program.translationKeyColumnName;
 }
 
-if (!program.localeColumnNames) {
-	localeColumnNames = program.localeColumnNames;
+if (program.localeColumnNames) {
+	localeColumnNames = JSON.parse(program.localeColumnNames);
 }
 
 importTranslations(program.inputFile, outputDir, outputFile, translationKeyColumnName, localeColumnNames);
 
+/**
+* Import translations
+* @param inputFile The input file
+* @param outputDir The output directory
+* @param outputFile The output file
+* @param translationKeyColumnName the translation key column name
+*/
 function importTranslations(inputFile, outputDir, outputFile, translationKeyColumnName, localeColumnNames) {
 
-	console.log(translationKeyColumnName);
+	console.log(chalk.blue('Importing translations using the following options:'));
+	console.log();
+	console.log(chalk.blue(`inputFile: ${inputFile}`));
+	console.log(chalk.blue(`outputDir: ${outputDir}`));
+	console.log(chalk.blue(`outputFile: ${outputFile}`));
+	console.log(chalk.blue(`translationKeyColumnName: ${translationKeyColumnName}`));
+	console.log(chalk.blue(`localeColumnNames: ${JSON.stringify(localeColumnNames)}`));
+	console.log();
+
+	console.log(`Importing translations from: ${inputFile}`);
 
 	fs.createReadStream(inputFile, {
 		encoding: BINARY_ENCODING
 	}).pipe(csvParser())
-		.on('headers', function (headers) {
+		.on('headers', (headers) => {
+			let hasTranslationKey = false;
 			for (let i = 0, len = headers.length; i < len; i++) {
-				if (headers[i] !== 'SYSTEM_KEY') {
-					translationsMap[headers[i].toLowerCase()] = {};
+				if (headers[i] !== translationKeyColumnName) {
+					translationsMap[getLocaleFromColumnName(headers[i], localeColumnNames)] = {};
+				} else {
+					hasTranslationKey = true;
 				}
 			}
-			//console.log(translationsMap);
+			if (!hasTranslationKey) {
+				throw new Error(chalk.red('There is no translation key column defined.'));
+			}
 		})
-		.on('data', function (data) {
+		.on('data',  (data) => {
 			for (let locale in translationsMap) {
 				if (translationsMap.hasOwnProperty(locale)) {
 					if (data[translationKeyColumnName]) {
-						translationsMap[locale][data[translationKeyColumnName]] = data[locale.toUpperCase()]
+						translationsMap[locale][data[translationKeyColumnName]] =
+						data[getColumnNameFromLocale(locale, localeColumnNames)]
 					}
 				}
 			}
 		})
-		.on('end', function () {
-
-			// check if output directory exists, if not create it.
-			if (!fs.existsSync(outputDir)) {
-				mkdirp(outputDir);
-			}
-
-			for (let locale in translationsMap) {
-				if (translationsMap.hasOwnProperty(locale)) {
-
-					const localeDirectoryPath = `${outputDir}/${locale}`;
-
-					// check if output directory exists, if not create it.
-					if (!fs.existsSync(localeDirectoryPath)) {
-						fs.mkdirSync(localeDirectoryPath);
-					}
-
-					const localeFilePath = `${outputDir}${locale}/${outputFile}`;
-
-					if (fs.existsSync(localeFilePath)) {
-						fs.unlinkSync(localeFilePath);
-					}
-
-					fs.writeFileSync(localeFilePath,
-						'export default ' + JSON.stringify(
-							unflatten(translationsMap[locale]), null, CHARACTER_SPACING), UTF_8_ENCODING);
-				}
-			}
+		.on('end',  () => {
+			generateTranslationFiles(translationsMap, outputDir, outputFile);
+			console.log();
+			console.log(chalk.green('Successfully imported translations.'));
 		});
 
+}
+
+/**
+* Returns the locale from the column name.
+* @param columnName The column name.
+* @param localeColumnNames The map of locale column names mapping.
+*/
+function getLocaleFromColumnName(columnName, localeColumnNames) {
+	if (localeColumnNames) {
+		for (let locale in localeColumnNames) {
+        	if (localeColumnNames.hasOwnProperty(locale) && columnName === localeColumnNames[locale]) {
+        		return locale;
+        	}
+        }
+	}
+	return columnName.toLowerCase();
+}
+
+/**
+ * Returns the column name for a locale.
+ * @param locale The locale
+ * @param localeColumnNames The map of locale column names passed by the command line.
+ * @returns {string}
+ */
+function getColumnNameFromLocale(locale, localeColumnNames) {
+	let columnName = localeColumnNames[locale];
+	if (!columnName) {
+		columnName = locale;
+	}
+	return columnName.toUpperCase();
+}
+
+/**
+* Generate translation files from translation map.
+* @param translationsMap The translation map.
+* @param outputDir The output directory.
+* @param outputFile The output file.
+*/
+function generateTranslationFiles(translationsMap, outputDir, outputFile) {
+
+	// check if output directory exists, if not create it.
+	if (!fs.existsSync(outputDir)) {
+		mkdirp(outputDir);
+	}
+
+	// loop each locale in translation map
+	for (let locale in translationsMap) {
+		if (translationsMap.hasOwnProperty(locale)) {
+
+			const localeDirectoryPath = `${outputDir}/${locale}`;
+
+				// check if output directory exists, if not create it.
+				if (!fs.existsSync(localeDirectoryPath)) {
+					fs.mkdirSync(localeDirectoryPath);
+				}
+
+				const localeFilePath = `${outputDir}/${locale}/${outputFile}`;
+
+				console.log(`Generating translations file: ${outputDir}/${locale}/${outputFile}`);
+
+				if (fs.existsSync(localeFilePath)) {
+					fs.unlinkSync(localeFilePath);
+				}
+
+				fs.writeFileSync(localeFilePath,
+					'export default ' + JSON.stringify(
+						unflatten(translationsMap[locale]), null, CHARACTER_SPACING), UTF_8_ENCODING);
+		}
+	}
 }
 
 
